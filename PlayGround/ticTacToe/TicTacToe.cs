30cd710569270e62f57f74213e840465c34134c7
Cortland <cortland.carrillo.dev@gmail.com>
2021-03-09 17:04:56 -0700

    started war class

--- PlayGround/ticTacToe/TicTacToe.cs
+++ PlayGround/ticTacToe/TicTacToe.cs
@@ -6,12 +6,34 @@ using System.Threading.Tasks;
 
 namespace PlayGround.ticTacToe {
     class TicTacToe : IPlayable {
+
+        private List<List<string>> board = new List<List<string>>();
+
         public void Play() {
+            populateEmptyBoard();
             getTicTacToeBoard();
         }
 
         private void getTicTacToeBoard() {
-            Console.WriteLine("tic tac toe board");
+            
+            foreach(List<string> row in board) {
+                foreach(string item in row) {
+                    Console.Write($"[{item}]");
+                }
+                Console.WriteLine();
+            }
         }
+
+        private void populateEmptyBoard() {
+            for (int i = 0; i < 3; i++) {
+                var list = new List<string>();
+                for(int j = 0; j < 3; j++) {
+                    list.Add("_");
+                }
+                board.Add(list);
+            }
+        }
+
+        
     }
 }
